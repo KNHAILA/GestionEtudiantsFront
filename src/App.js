c5db2935e8fb0a512fff9ca1cf655554a1e56b71
GH-Mina <you@example.com>
2021-01-24 18:12:37 +0100

    merge navBars

--- src/App.js
+++ src/App.js
@@ -3,32 +3,40 @@ import Login from './Login/login'
 
 
 import EmploiTemps from './Internaute/EmploiTemps/EmploiTemps';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
 import CoursSearch from './Etudiants/Cours/CoursSearch';
 import CoursListe from './Etudiants/Cours/CoursListe';
 import Service from './Etudiants/Service/Service';
-import NavBarInternaute from './Internaute/NavBarInternaute/NavBarInternaute';
 import Acceuil from './Internaute/Acceuil/Acceuil';
-import NavBarStudent from './Etudiants/NavBarStudent/NavBarStudent';
-import Notes  from './Etudiants/Statistiques/notes'
-import Chart  from './Etudiants/Statistiques/absences'
+import NavBar from './NavBar/NavBar';
+import Notes from './Etudiants/Statistiques/notes'
+import Chart from './Etudiants/Statistiques/absences'
+import GestionCours from './Admin/Cours/GestionCours'
 import TraitementDemandes from './Admin/Service/TraitementDemandes'
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+
+    <BrowserRouter>
+      <div className="App">
         {/* <Route exact path='/' component={NavBarInternaute} />*/}
-        <Route exact path='/' component={NavBarStudent} />
-        <Route exact path='/Login' component={Login} />
-        <Route exact path='/EmploiDuTemps' component={EmploiTemps} />
-        <Route exact path='/AcceuilInternaute' component={Acceuil} />
-        <Route exact path='/Services' component={Service} />
-        <Route exact path='/ListeCours' component={CoursListe} />
-        <Route exact path='/ChercherCours' component={CoursSearch} />
-        <Route exact path='/Notes' component={Notes}/>
-        <Route exact path='/Absences' component={Chart}/>
-      </BrowserRouter>
-    </div>
+        <NavBar user={"student"}/>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route exact path='/EmploiDuTemps' component={EmploiTemps} />
+            <Route exact path='/AcceuilInternaute' component={Acceuil} />
+            <Route exact path='/Services' component={Service} />
+            <Route exact path='/ListeCours' component={CoursListe} />
+            <Route exact path='/ChercherCours' component={CoursSearch} />
+            <Route exact path='/Notes' component={Notes} />
+            <Route exact path='/GestionCours' component={GestionCours} />
+
+            <Route exact path='/serviceAdmin' component={TraitementDemandes} />
+          <Route exact path='/Absences' component={Chart} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
